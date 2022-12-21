68cb3ce92d94786e4aaae97963c0a62ebfcade6f
Richard <tuned_in@outlook.com>
2022-12-21 19:15:56 +1100

    writing basic struct of content.js

--- chromeExtension/background.js
+++ chromeExtension/background.js
@@ -1,22 +1,38 @@
-// chrome.runtime.message
+
 
 // connect with the content.js page
 chrome.runtime.onMessage.addListener( 
-  function(request, response) {
-    if (request.heading == "something") {
+  function(request) {
+    if (request['heading'] != NULL) {
       // make a handler later on
       const api_key = chrome.storage.sync.get('api_key');
       // promises
       // if (api_key is invalid, then reject it and error message otherwise resole)
     }
 
-    const openai = " url"
     // do a post request and get a response back from openai (hint)
 
     // send this response back to the content.js 
     //do chrome scripting in scipt.js
   }
 )
+
+function process_opnenai() {
+
+  const openAiUrl = " url"
+  const data = {
+    "model":request.paragraph,
+    "prompts": {tokens, etc}
+  };
+
+  try {
+    const response = fetch(url, method: 'POST', {resquest:body})
+  }
+  catch (err) {
+
+  }
+
+}
 
 
 
--- chromeExtension/content.js
+++ chromeExtension/content.js
@@ -1,4 +1,4 @@
-/**
+/*
  * Get the description f question on leetcode page
  */
 
@@ -6,7 +6,7 @@
 // content gets the q from leetcode page
 // stores the content
 
-// sends a message to background.js like ("transfer system")
+// sends a message to background.js like request[{'heading: done'}, {response: "(the leetcode question)"}]
 
 // onConnect() // finished with parsing the quesiton
 // send a message saying something done with scraping leetcode
