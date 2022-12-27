69834f34a02eaae5c3eb33e232c626499cd3d676
David Zhou <zhouyi021022@gmail.com>
2022-12-27 16:41:45 +1100

    Debug background message

--- chromeExtension/background.js
+++ chromeExtension/background.js
@@ -3,6 +3,8 @@
 // connect with the content.js page
 chrome.runtime.onMessage.addListener( 
   function(request, response) {
+    console.log("Get the request");
+
     if (request.heading == "something") {
       // make a handler later on
       const api_key = chrome.storage.sync.get('api_key');
@@ -14,22 +16,19 @@ chrome.runtime.onMessage.addListener(
     // do a post request and get a response back from openai (hint)
 
     // tabId is the unique identifier for the tab
-    console.log(tab.url);
-    if (leetcodeExp.test(tab.url) && changeInfo.status === "complete") {
-      // console.log("Check");
-      chrome.scripting
-        .executeScript({
-            target: {
-                tabId: tabId,
-            },
-            files: ["./foreground.js"], 
-      }).then((result) => {
-          console.log(`Parsed result ${JSON.stringify(result)}`);
-      });
+    // console.log("Check");
+    /* chrome.scripting
+      .executeScript({
+        target: {
+          tabId: tabId,
+        },
+        files: ["./foreground.js"], 
+    }).then((result) => {
+        console.log(`Parsed result ${JSON.stringify(result)}`);
+    }); */
 
-      // send this response back to the content.js 
-      // do chrome scripting in scipt.js
-    }
+    // send this response back to the content.js 
+    // do chrome scripting in scipt.js
   }
 )
 
