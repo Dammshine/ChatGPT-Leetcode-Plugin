b129f98a9d13ff985fb0fe79e6cbd9f8d812c2fc
David Zhou <zhouyi021022@gmail.com>
2022-12-30 21:45:30 +1100

    Debug

--- chromeExtension/background.js
+++ chromeExtension/background.js
@@ -2,50 +2,47 @@
 const openAiUrl = 'https://api.openai.com/v1/edits'
 
 chrome.runtime.onMessage.addListener( 
-  function(request) {
-    if (request['heading'] != NULL) {
+  async function(request) {
+    console.log(request);
+    // A hint request
+    if (request['message_type'] !== undefined) {
+      console.log(request.tabId);
       // make a handler later on
-      const api_key = chrome.storage.sync.get('api_key');
+      const api_key = await chrome.storage.sync.get('api_key');
+      
       // doing promises later
-      if (!api_key) {
+      if (api_key === {}) {
         console.error('Could not get API key');
       } else {
-        const res = process_opnenai(api_key);
-        if (res.response.statusCode === 200) {
-          chrome.storage.sync.set('hint', res.response.body);
+        // console.log(api_key);
+        const res = await process_opnenai(api_key.api_key);
+        console.log(api_key);
+
+        if (res.error !== undefined) {
+          
         } else {
-          console.error('Could not get hint');
+          await chrome.storage.sync.set('hint', res['choices'][0]);
+
+          chrome.scripting
+            .executeScript({
+              target: {
+                tabId: request.tabId,
+              },
+              files: ["./foreground.js"], 
+          }).then((result) => {
+            console.log(`Parsed result ${JSON.stringify(result)}`);
+          });
         }
       }
       // promises
       // if (api_key is invalid, then reject it and error message otherwise resole)
     }
-
-    // send this response back to foreground.js
-    //do chrome scripting in scipt.js
-    // chrome.tabs.sendMessage()
-
-    const openai = " url"
-    // do a post request and get a response back from openai (hint)
-
-    // tabId is the unique identifier for the tab
-    // console.log("Check");
-    /* chrome.scripting
-      .executeScript({
-        target: {
-          tabId: tabId,
-        },
-        files: ["./foreground.js"], 
-    }).then((result) => {
-        console.log(`Parsed result ${JSON.stringify(result)}`);
-    }); */
-
     // send this response back to the content.js 
     // do chrome scripting in scipt.js
   }
 )
 
-function preprocess_question(paragraph) {
+/*function preprocess_question(paragraph) {
   // if 
   var question = paragraph.replace(/<[^>]*>/g, "");
   question = question.replace('&nbsp;, ""');
@@ -61,17 +58,21 @@ function getQuestion() {
 }
 
 // do a post request and get a response back from openai (hint)
+*/
+
 async function process_opnenai(api_key) {
-  leet_q = getQuestion();
+  leet_q = `Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+  You may assume that each input would have exactly one solution, and you may not use the same element twice.
+  You can return the answer in any order.`;
 
   const request_body = {
-    "model": "text-davinci:003",
-    "inputs": leet_q,
+    "model": "text-davinci-edit-001",
+    "input": leet_q,
     "instruction": "Give me an verbal answer"
   };
 
   try {
-    const response = fetch(url, {
+    const response = fetch(openAiUrl, {
       method: 'POST', 
       headers: {
         'Content-Type': 'application/json',
@@ -79,14 +80,12 @@ async function process_opnenai(api_key) {
       },
       body: JSON.stringify(request_body)
     });
-    return (await response).json;
+    return (await response).json();
 
   } catch (err) {
     console.log(err);
     return -1;
   };
-
 }
-
 
 
